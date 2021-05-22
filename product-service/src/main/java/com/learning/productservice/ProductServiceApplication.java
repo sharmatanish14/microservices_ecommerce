f5ff1ed9ec8cf898cd58ccbd9dc14fc12601dff0
Tanish <no email>
2021-05-22 19:57:48 +0530

    add git config client in product-service and order-service

--- product-service/src/main/java/com/learning/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/learning/productservice/ProductServiceApplication.java
@@ -1,12 +1,16 @@
 package com.learning.productservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class ProductServiceApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);

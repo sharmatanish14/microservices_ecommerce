bc3b87bf8936ed8ed2f00fdf02ccd180f4fc3923
Tanish <no email>
2021-05-23 20:23:21 +0530

    add hashicorp vault and move uri username and password from file to vault

--- product-service/src/main/java/com/learning/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/learning/productservice/ProductServiceApplication.java
@@ -11,9 +11,16 @@ import javax.annotation.PostConstruct;
 @EnableEurekaClient
 public class ProductServiceApplication {
 
+	@Value("${spring.data.mongodb.uri}")
+	String database_uri;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	@PostConstruct
+	public void post(){
+		System.out.println("$$$$$$$$$$ Able to access property from vault");
+		System.out.println(database_uri);
+	}
 }

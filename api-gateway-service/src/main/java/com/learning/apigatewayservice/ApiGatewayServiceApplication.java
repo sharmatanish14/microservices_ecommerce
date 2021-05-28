e8c15e5d9ad903509cf7552da9d5af7f7da35153
Tanish <no email>
2021-05-28 11:15:05 +0530

    api gateway configuration

--- api-gateway-service/src/main/java/com/learning/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/learning/apigatewayservice/ApiGatewayServiceApplication.java
@@ -2,8 +2,11 @@ package com.learning.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {

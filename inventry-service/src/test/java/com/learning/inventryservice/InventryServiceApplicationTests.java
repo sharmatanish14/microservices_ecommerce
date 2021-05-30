31c736974bc57664f7cac2b1734c08b197a19997
Tanish <no email>
2021-05-30 08:58:55 +0530

    add inventry service skeleton

---
+++ inventry-service/src/test/java/com/learning/inventryservice/InventryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.inventryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

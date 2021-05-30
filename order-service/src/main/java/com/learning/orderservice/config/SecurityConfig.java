c87306ddd74cf1ba1c9a7987e731a3bd8a903c12
Tanish <no email>
2021-05-30 20:12:11 +0530

    add resilience4j circuit breaker in order service

---
+++ order-service/src/main/java/com/learning/orderservice/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.learning.orderservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @PostConstruct
+    public void enableAuthenticationOnSpawnedThreads(){
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
+}
--- order-service/src/main/java/com/learning/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/learning/orderservice/controller/OrderController.java
@@ -5,9 +5,12 @@ import com.learning.orderservice.dto.OrderDto;
 import com.learning.orderservice.model.Order;
 import com.learning.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/api/order")
@@ -16,12 +19,17 @@ public class OrderController {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final Resilience4JCircuitBreakerFactory resilience4JCircuitBreakerFactory;
+    
 
     @PostMapping
     public String placeOrder(@RequestBody OrderDto orderDto){
 
+        Resilience4JCircuitBreaker circuitBreaker = resilience4JCircuitBreakerFactory.create("inventory");
+        Supplier<Boolean> booleanSupplier =()->orderDto.getOrderLineItemsList().stream().allMatch(orderLineItem -> inventoryClient.checkStock(orderLineItem.getSkuCode()));
+        Boolean allProductInStock = circuitBreaker.run(booleanSupplier, throwable -> handleErrorCase());
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@  Order request received");
-        boolean allProductInStock = orderDto.getOrderLineItemsList().stream().allMatch(orderLineItem -> inventoryClient.checkStock(orderLineItem.getSkuCode()));
+
 
         if(allProductInStock){
             Order order=new Order();
@@ -35,6 +43,10 @@ public class OrderController {
             return "order failed please try again";
         }
 
+    }
+
+    private Boolean handleErrorCase() {
+        return false;
     }
 
     @GetMapping
